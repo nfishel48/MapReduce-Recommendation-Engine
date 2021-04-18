e96a60991b62fd1c09ef5460b6bb436ae7706466
nfishel <n-fishel_v@dcunet.org>
2021-04-18 19:45:23 -0400

    sort method working

--- Modules/Module1.py
+++ Modules/Module1.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from itertools import combinations
 
 class Phase1:
 
@@ -15,6 +16,37 @@ class Phase1:
 
     def reduce(self):
         self.K3 = pd.DataFrame({'Userid': self.V2.Userid})
-        self.V3 = pd.concat([self.K2.Movie, self.V2.Rating, self.K2.groupby(by='Movie')['Movie'].transform('count')],axis=1,keys=['Movie', 'Rating', '#Ratings'])
+        self.V3 = pd.concat([self.K2.Movie, self.V2.Rating, self.K2.groupby(by='Movie')['Movie'].transform('count')],axis=1,keys=['Movie', 'Rating', 'NumOfRatings'])
         return(self.K3, self.V3)
+
+class Phase2:
+
+    def __init__(self, K3, V3):
+        self.K3 = K3
+        self.V3 = V3
+
+    def map(self):
+        horizontal_concat = pd.concat([self.K3, self.V3], axis=1)
+        t3 = list(horizontal_concat.itertuples(index=False))
+        return t3
+
+    def sort(self, t3): #on a cluster this would send the data to diffent nodes before the reduce
+        maxUserId = self.K3.Userid.max()
+        d3 = dict()
+        i = 0
+        while i < len(t3): #Go through the list and create a dictionary with all the values tied to the same user
+            print(t3[i].Userid)
+            key = t3[i].Userid
+            if key in d3: #if this user id is already in the dict add the values to this key
+                d3[t3[i].Userid].append([t3[i].Movie,t3[i].Rating,t3[i].NumOfRatings])
+            else: #Create a new key and give values to that key
+                d3[t3[i].Userid] = [t3[i].Movie,t3[i].Rating,t3[i].NumOfRatings]
+            i = i + 1
+        return d3
+
+    def reduce(self, d3):
+        t4 = list(combinations(t3, 2))
+        
+
+        
         
